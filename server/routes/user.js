2a85b58af61d10a4d55b6c087a1390c01be95993
Lara <larabernspereira@gmail.com>
2024-03-08 02:15:21 -0300

    auth logic with server implemented

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import { User } from '../models/User.js'
 import jwt from 'jsonwebtoken'
+import nodemailer from 'nodemailer'
 
 const router = express.Router()
 
@@ -45,9 +46,89 @@ router.post("/login", async (req, res) => {
     }
 
     const token = jwt.sign({ username: user.username }, process.env.KEY, { expiresIn: '1h' })
-    res.cookie('token', token, {httpOnly: true, maxAge: 360000 })
+    res.cookie('token', token, { httpOnly: true, maxAge: 360000 })
 
     return res.json({ status: true, message: "Login com sucesso" })
 })
+
+
+// Route for user Forgot Password
+router.post("/forgotPassword", async (req, res) => {
+    const { email } = req.body
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        return res.json({ message: 'Usuário não cadastrado' })
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.KEY, { expiresIn: '5m' })
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.SENHAEMAIL1 + ' ' + process.env.SENHAEMAIL2 + ' ' + process.env.SENHAEMAIL3 + ' ' + process.env.SENHAEMAIL4
+        }
+    });
+
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'Redefinir Senha',
+        text: `http://localhost:5173/resetPassword/${token}`
+    };
+
+    transporter.sendMail(mailOptions, function (error) {
+        if (error) {
+            return res.json({ message: "Erro ao enviar email" })
+        } else {
+            return res.json({ status: true, message: "Email enviado" })
+        }
+    });
+
+})
+
+// Route for user Reset Password
+router.post("/resetPassword/:token", async (req, res) => {
+    const { token } = req.params
+    const { password } = req.body
+
+    try {
+        const decoded = await jwt.verify(token, process.env.KEY)
+        const id = decoded.id
+        const hashpassword = await bcrypt.hash(password, 10)
+        await User.findByIdAndUpdate({ _id: id }, { password: hashpassword })
+        return res.json({ status: true, message: 'Senha atualizada' })
+    } catch (error) {
+        res.json({ message: 'Token invalido' })
+    }
+})
+
+const verifyUser = async (req, res, next) => {
+
+    try {
+        const token = req.cookies.token
+        if (!token) {
+            return res.json({ status: false, message: "Sem token" })
+        }
+        const decoded = await jwt.verify(token, process.env.KEY)
+        next()
+    } catch (error) {
+        return res.json(error)
+    }
+
+}
+
+// Protected routers
+router.get('/verify', verifyUser, (req, res) => {
+    return res.json({ status: true, message: "Autorizado" })
+})
+
+// Logout route
+router.get('/logout', (req, res) => {
+    res.clearCookie('token')
+    return res.json({ status: true })
+})
+
 
 export { router as UserRouter }
